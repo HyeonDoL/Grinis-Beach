2ec3acdf64c41c41d453d0eddd0752eaf36970f0
정지상 <ashuatz@gmail.com>
2017-06-05 01:53:34 +0900

    [UI] -Add AutoFade -Add EasyClick Classes

---
+++ Grinis Beach/Assets/Script/UI/EasyClick/UI_Click.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(EventTrigger))]
+public abstract class UI_Click : MonoBehaviour
+{
+    public abstract void OnClick(BaseEventData bed);
+
+    public virtual void SetTrigger(UnityAction<BaseEventData> call)
+    {
+        EventTrigger.Entry MyEntry = new EventTrigger.Entry();
+        MyEntry.eventID = EventTriggerType.PointerClick;
+        MyEntry.callback.AddListener(call);
+        this.GetComponent<EventTrigger>().triggers.Add(MyEntry);
+    }
+}
