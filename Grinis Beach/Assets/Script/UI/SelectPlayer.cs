7151f6786f41442056984df9755b8ebe202f7e70
정지상 <ashuatz@gmail.com>
2017-06-05 02:14:16 +0900

    [scene] -create menu

---
+++ Grinis Beach/Assets/Script/UI/SelectPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private int index;
+
+    [SerializeField]
+    private GameObject[] Units;
+
+    void Awake()
+    {
+        index = 0;
+        this.SelectedUnitUpdate();
+    }
+
+    public void OnClickArrow(int add)
+    {
+        index += Mathf.RoundToInt(Mathf.Sign(add));
+        index %= Units.Length;
+        SelectedUnitUpdate();
+    }
+
+    private void SelectedUnitUpdate()
+    {
+        for (int i = 0; i < Units.Length; ++i) Units[i].SetActive(index == i ? true : false);
+    }
+}
