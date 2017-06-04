372b3516ebc8a863933b0b7fb37c39253cbc9501
정지상 <ashuatz@gmail.com>
2017-06-05 01:39:27 +0900

    타이틀제작중~

---
+++ Grinis Beach/Assets/Script/UI/UI_AlphaPingPong.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_AlphaPingPong : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.UI.Text myText;
+    private Color TextColor;
+    private float Alpha;
+    private WaitForEndOfFrame waitForEndOfFrame;
+
+    void Awake()
+    {
+        TextColor = myText.color;
+        waitForEndOfFrame = new WaitForEndOfFrame();
+        StartCoroutine(AlphaPingPong());
+    }
+
+    private IEnumerator AlphaPingPong()
+    {
+        while (true)
+        {
+            myText.color = new Color(TextColor.r, TextColor.g, TextColor.b, Mathf.PingPong(Time.time, 1));
+            yield return waitForEndOfFrame;
+        }
+
+    }
+}
