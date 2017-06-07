cb0c2ae0eb0272d6a346d06823483eadc9634131
HyunDo_Lee <godd7776@gmail.com>
2017-06-08 03:23:17 +0900

    [Script] PathMove, BezierSplinePath, CustomInspector, AutoDestroyParticle, WaterBomb
    [Model]
    Particles

---
+++ Grinis Beach/Assets/Script/Particle/AutoDestroyParticle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyParticle : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem ps;
+
+    [SerializeField]
+    private GameObject destroyObject;
+
+    void Update()
+    {
+        if (ps)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(destroyObject);
+            }
+        }
+    }
+}
