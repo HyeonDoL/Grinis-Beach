2fbc2d90acc805652fd11c8156e30042b72cafbc
정지상 <ashuatz@gmail.com>
2017-06-07 09:35:23 +0900

    [script] -add audioManagement script -add audio(source,clip) container
    [prefab]
    -add fast-get AudioContainer

---
+++ Grinis Beach/Assets/Script/DataContainer/AudioClipContainer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClipContainer : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] m_Clips;
+
+    public AudioClip this[int index]
+    {
+        get
+        {
+            return m_Clips[index];
+        }
+    }
+    void Awake()
+    {
+        AudioManager.Instance.audioClipContainer_readonly = this;
+    }
+}
---
+++ Grinis Beach/Assets/Script/Manager/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+
+    private static AudioManager instance = null;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance)
+                return instance;
+            else
+            {
+                instance = new GameObject("AudioManager").AddComponent<AudioManager>();
+                instance.transform.parent = Camera.main.transform;
+                return instance;
+            }
+        }
+    }
+
+    #region readonly
+
+    private AudioClipContainer audioClipContainer = null;
+    public AudioClipContainer audioClipContainer_readonly
+    {
+        get { return audioClipContainer; }
+        set { if (!audioClipContainer) audioClipContainer = value; }
+    }
+
+    private AudioSourceContainer audioSourceContainer = null;
+    public AudioSourceContainer audioSourceContainer_readonly
+    {
+        get { return audioSourceContainer; }
+        set { if (!audioSourceContainer) audioSourceContainer = value; }
+    }
+
+    #endregion
+
+    public void DoMyBestPlay(int index)
+    {
+        int SourceContainerLen = audioSourceContainer.Length;
+        int NotPlayingSourceIndex = SourceContainerLen - 1;
+
+        for (int i = 0; i < SourceContainerLen; ++i)
+        {
+            if (!audioSourceContainer[i].isPlaying)
+            {
+                NotPlayingSourceIndex = i;
+                break;
+            }
+        }
+
+        audioSourceContainer[NotPlayingSourceIndex].clip = audioClipContainer[index];
+
+        audioSourceContainer[NotPlayingSourceIndex].Play();
+
+    }
+}
